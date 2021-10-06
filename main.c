4f2cae817eac6314e031b9222a059d5f8e57aeb8
jennisun <jsun20@stuy.edu>
2021-10-06 19:31:47 -0400

    mystrncpy

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "main.h"
 
 int mystrlen( char *s ) {
@@ -9,8 +12,32 @@ int mystrlen( char *s ) {
   return i;
 }
 
+char * mystrncpy( char *dest, char *source, int n) {
+  int i = 0;
+  for (i = 0; i < n; i ++) {
+    *(dest + i) = *(source + i);
+  }
+  *(dest + n) = '\0';
+  return dest;
+}
+
 int main() {
+  char s1[100];
   char s2[100] = "hello";
-  printf("[standard]:\t%d\n", mystrlen(s2) );
+  char s3[100] = "goodbye";
+
+  printf("start strings:\n");
+  printf("s1: [%s]\n", s1);
+  printf("s2: [%s]\n", s2);
+  printf("s3: [%s]\n", s3);
+
+  printf("\nTesting strlen(s2):\n");
+  printf("[standard]:\t%lu\n", strlen(s2) );
+  printf("[mine]:\t\t%d\n", mystrlen(s2) );
+
+  printf("\nTesting strncpy(s1, s3, 3):\n");
+  printf("[standard]:\t[%s]\n", strncpy(s1, s3, 3) );
+  printf("[mine]:\t\t[%s]\n", mystrncpy(s1, s3, 3) );
+
   return 0;
 }
